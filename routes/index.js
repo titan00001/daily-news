960ad9673874cd87e4b9171e5ad99c3a67e4eb05
titan00001 <gplay9792@gmail.com>
2020-01-28 22:00:05 +0530

    Added customized end-points

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-const getTOINews = require("../public/data/getNews")
+const {getNews, getFilteredNews } = require("../public/data/getNews")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    news = getTOINews()
+    news = getNews()
     res.render('index', { title: 'Daily News', newsJson: news });
   
 });
+
+router.get('/politics', function(req, res, next) {
+
+    news = getNews()
+    news = getFilteredNews(news["timesofindia.indiatimes.com"], ["india", "elections", "world", "business", "nri"])
+    res.render('index', { title: 'Political News', newsJson: news });
+
+})
+
+router.get('/city', function(req, res, next) {
+
+  news = getNews()
+  res.render('index', { title: 'City News', newsJson: news["timesofindia.indiatimes.com"]["city"] });
+
+})
 
 module.exports = router;
 
